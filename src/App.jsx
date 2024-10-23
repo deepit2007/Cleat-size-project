112ad74e2effb2fc859c409790271734f86b5c20
deepit2007 <regdeepit@outlook.com>
2024-10-23 17:21:23 -0500

    Save all changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import FAQ from './pages/FAQ';
+import Charts from './pages/Charts';
 import Nav from './components/nav/navComponent';
-const App = () => {
+
+function App() {
   return (
-    <div>
-      <Nav/>
-    </div>
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/FAQ" element={<FAQ />} />
+        <Route path="/Charts" element={<Charts />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App
+ export default App;
--- src/components/nav/navComponent.jsx
+++ src/components/nav/navComponent.jsx
@@ -1,18 +1,20 @@
 import '../../index.css';
 import './navComponent.css';
+import { Link } from 'react-router-dom';
+
 const Nav = () => {
   return (
     <div className='Navbar'>
       
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>FAQs</li>
-        <li>Charts</li>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/About">About</Link></li>
+      <li><Link to="/FAQ">FAQ</Link></li>
+      <li><Link to="/Charts">Charts</Link></li>
       </ul>
       
     </div>
   );
 }
 
-export default Nav
+export default Nav;
